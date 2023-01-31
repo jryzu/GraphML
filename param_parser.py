98db00630b818e9e90d485247d8784cacfc68fd7
Jerry Zhu <62519176+jryzu@users.noreply.github.com>
2023-01-31 03:38:58 -0500

    Update param_parser.py

--- param_parser.py
+++ param_parser.py
@@ -2,12 +2,7 @@
 import argparse
 
 def parameter_parser():
-    """
-    A method to parse up command line parameters.
-    By default it gives an embedding of the partial NCI1 graph dataset.
-    The default hyperparameters give a good quality representation without grid search.
-    Representations are sorted by ID.
-    """
+
     parser = argparse.ArgumentParser(description="Run Graph2Vec.")
 
     parser.add_argument("--input-path",
