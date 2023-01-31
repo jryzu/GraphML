ae3713d5999e42acd24824888ac9abe4196c9c38
Jerry Zhu <62519176+jryzu@users.noreply.github.com>
2023-01-31 03:38:44 -0500

    Update graph2vec.py

--- graph2vec.py
+++ graph2vec.py
@@ -10,16 +10,9 @@ from param_parser import parameter_parser
 from gensim.models.doc2vec import Doc2Vec, TaggedDocument
 
 class WeisfeilerLehmanMachine:
-    """
-    Weisfeiler Lehman feature extractor class.
-    """
+
     def __init__(self, graph, features, iterations):
-        """
-        Initialization method which also executes feature extraction.
-        :param graph: The Nx graph object.
-        :param features: Feature hash table.
-        :param iterations: Number of WL iterations.
-        """
+
         self.iterations = iterations
         self.graph = graph
         self.features = features
@@ -28,10 +21,7 @@ class WeisfeilerLehmanMachine:
         self.do_recursions()
 
     def do_a_recursion(self):
-        """
-        The method does a single WL recursion.
-        :return new_features: The hash table with extracted WL features.
-        """
+
         new_features = {}
         for node in self.nodes:
             nebs = self.graph.neighbors(node)
@@ -45,9 +35,7 @@ class WeisfeilerLehmanMachine:
         return new_features
 
     def do_recursions(self):
-        """
-        The method does a series of WL recursions.
-        """
+
         for _ in range(self.iterations):
             self.features = self.do_a_recursion()
 
@@ -56,13 +44,7 @@ def path2name(path):
     return os.path.splitext(base)[0]
 
 def dataset_reader(path):
-    """
-    Function to read the graph and features from a json file.
-    :param path: The path to the graph json.
-    :return graph: The graph object.
-    :return features: Features hash table.
-    :return name: Name of the graph.
-    """
+
     name = path2name(path)
     data = json.load(open(path))
     graph = nx.from_edgelist(data["edges"])
@@ -77,25 +59,14 @@ def dataset_reader(path):
     return graph, features, name
 
 def feature_extractor(path, rounds):
-    """
-    Function to extract WL features from a graph.
-    :param path: The path to the graph json.
-    :param rounds: Number of WL iterations.
-    :return doc: Document collection object.
-    """
+
     graph, features, name = dataset_reader(path)
     machine = WeisfeilerLehmanMachine(graph, features, rounds)
     doc = TaggedDocument(words=machine.extracted_features, tags=["g_" + name])
     return doc
 
 def save_embedding(output_path, model, files, dimensions):
-    """
-    Function to save the embedding.
-    :param output_path: Path to the embedding csv.
-    :param model: The embedding model object.
-    :param files: The list of files.
-    :param dimensions: The embedding dimension parameter.
-    """
+
     out = []
     for f in files:
         identifier = path2name(f)
@@ -106,11 +77,7 @@ def save_embedding(output_path, model, files, dimensions):
     out.to_csv(output_path, index=None)
 
 def main(args):
-    """
-    Main function to read the graph list, extract features.
-    Learn the embedding and save it.
-    :param args: Object with the arguments.
-    """
+
     graphs = glob.glob(os.path.join(args.input_path, "*.json"))
     print("\nFeature extraction started.\n")
     document_collections = Parallel(n_jobs=args.workers)(delayed(feature_extractor)(g, args.wl_iterations) for g in tqdm(graphs))
